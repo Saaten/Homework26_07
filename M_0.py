e06f21e797c4e5a62a393fdc8ca728a21a210e6f
Saaten <136030812+Saaten@users.noreply.github.com>
2023-07-29 22:59:11 +0400

    M_0.py

--- M_0.py
+++ M_0.py
@@ -1,25 +1,6 @@
 ﻿
 '''Փոխել 0-ները հարևան M-երի քանակով '''
 
-'''import random
-list=['M','0']
-x=[]
-for i in range(0,5):
-    for j in range(0,5):
-        num=x.append(random.choice(list))
-        print (num, end="")
-    print("")
-
-for i in range(0,5):
-    for j in range(0,5):
-        if x[i][j]!='M':
-            for i1 in range(-1,2):
-                for j1 in range(-1,2):
-                    if i+i1>=0 and i+i1<=4 and j+j1>=0 and j+j1<=4 and x[i+i1][j+j1]=='M':
-                        x[i][j]+=1
-        print(x[i][j],end="")
-    print("") '''
-
 x=[[0,'M',0,'M',0],
    [0,0,'M',0,0],
    [0,0,0,0,0],
